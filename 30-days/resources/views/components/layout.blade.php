151720b5ff2306b33154e6fd4268e7ca74faa32f
Tom Emming <tom@tinux-it.nl>
2025-04-28 22:09:35 +0200

    feat: laracasts-30-day-05

--- 30-days/resources/views/components/layout.blade.php
+++ 30-days/resources/views/components/layout.blade.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}" class="h-full bg-gray-100">
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -16,15 +16,7 @@
         @vite(['resources/css/app.css', 'resources/js/app.js'])
     @endif
 </head>
-<body>
-<!--
-  This example requires updating your template:
-
-  ```
-  <html class="h-full bg-gray-100">
-  <body class="h-full">
-  ```
--->
+<body class="h-full antialiased leading-none font-sans bg-gray-100">
 <div class="min-h-full">
     <nav class="bg-gray-800">
         <div class="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
@@ -36,9 +28,9 @@
                     <div class="hidden md:block">
                         <div class="ml-10 flex items-baseline space-x-4">
                             <!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" -->
-                            <a href="/" class="rounded-md bg-gray-900 px-3 py-2 text-sm font-medium text-white" aria-current="page">Home</a>
-                            <a href="/about" class="rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white">About</a>
-                            <a href="/contact" class="rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white">Contact</a>
+                            <x-nav-link href="/" type="a" :active="request()->is('/')">Home</x-nav-link>
+                            <x-nav-link href="/about" type="b" :active="request()->is('about')" >About</x-nav-link>
+                            <x-nav-link href="/contact" type="a" :active="request()->is('contact')">Contact</x-nav-link>
                         </div>
                     </div>
                 </div>
--- 30-days/resources/views/components/nav-link.blade.php
+++ 30-days/resources/views/components/nav-link.blade.php
@@ -1,1 +1,18 @@
-<a {{ $attributes }}">{{ $slot }}</a>
+@props(['active' => false])
+@props(['type' => 'a'])
+
+@if ($type == 'a')
+<a {{ $attributes}} class="{{ $active ? "bg-gray-900 text-white" : "text-gray-300 hover:bg-gray-700 hover:text-white" }} rounded-md px-3 py-2 text-sm font-medium"
+   aria-current="{{ $active ? 'page' : 'false' }}"
+>
+    {{ $slot }}
+</a>
+@elseif ($type == 'b')
+<button {{ $attributes}} class="{{ $active ? "bg-gray-900 text-white" : "text-gray-300 hover:bg-gray-700 hover:text-white" }} rounded-md px-3 py-2 text-sm font-medium"
+   aria-current="{{ $active ? 'page' : 'false' }}"
+>
+    {{ $slot }}
+</button>
+@endif
+
+
