b07b88ac9bbf730696ac4ae6645ba4b47f3832e6
Tom Emming <tom@tinux-it.nl>
2025-04-30 21:54:19 +0200

    feat: laracasts-30-day-26

--- 30-days/resources/views/components/layout.blade.php
+++ 30-days/resources/views/components/layout.blade.php
@@ -5,15 +5,13 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
     <title>Laravel</title>
-
     <!-- Fonts -->
     <link rel="preconnect" href="https://fonts.bunny.net">
     <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet" />
-    <script src="https://cdn.tailwindcss.com"></script>
 
     <!-- Styles / Scripts -->
     @if (file_exists(public_path('build/manifest.json')) || file_exists(public_path('hot')))
-        @vite(['resources/css/app.css', 'resources/js/app.js'])
+        @vite(['resources/js/app.js'])
     @endif
 </head>
 <body class="h-full antialiased leading-none font-sans bg-gray-100">
